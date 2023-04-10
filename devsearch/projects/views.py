07a02889609ee4e35cae0b4179490caeab6c279f
Maciej Szefler <49917065+rysiekpol@users.noreply.github.com>
2023-04-10 23:56:30 +0200

    Added basic database models

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,34 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project
 
-projectList = [
-    {
-    'id': '1',
-    'title': 'Ecommerce Website',
-    'description': 'Fully functional ecommerce website'
-    },
-    {
-    'id': '2',
-    'title': 'Portfolio Website',
-    'description': 'This was a project where I built out my portfolio website'
-    },
-    {
-    'id': '3',
-    'title': 'Social Network',
-    'description': 'Awesome open source project I am still working on'
-    },
-]
 
 def projects(request):
-    page = 'projects'
-    number = 10
-    context = {'page': page, 'number': number, 'projects': projectList}
+    projectsObj = Project.objects.all()
+    context = {'projects': projectsObj}
     return render(request, 'projects/projects.html', context)
 
 
 def project(request, pk):
-    projectObj = None
-    for project in projectList:
-        if project['id'] == pk:
-            projectObj = project
+    projectObj = Project.objects.get(id=pk)
     return render(request, 'projects/single_project.html', {'project': projectObj})
